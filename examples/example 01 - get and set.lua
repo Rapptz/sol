3a6207a8d118360529f3503df64d0ae820383bd4
ThePhD <phdofthehouse@gmail.com>
2014-04-24 08:21:20 -0400

    Removing old examples

--- examples/example 01 - get and set.lua
+++
@@ -1,3 +0,0 @@
-local variable1 = 0
-local variable2 = "A string"
-local variable3 = 2.3
--- examples/example 02 - functions.lua
+++
@@ -1,9 +0,0 @@
-local woof = 0
-
-function subtract_and_hello(a, b)
-    return a - b, "hello"
-end
-
-function change_woof ()
-    woof = woof + 1
-end
